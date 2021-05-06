ed478737c315b7366b06cecf43476b2473a185eb
magralo <magralo@gmail.com>
2021-05-05 21:52:03 -0500

    initial comit

---
+++ Dash/app1.py
@@ -0,0 +1,59 @@
+import os
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
+from flask import Flask
+
+server = Flask(__name__)
+
+
+app = dash.Dash(server=server)
+
+df = pd.read_csv(
+    'https://gist.githubusercontent.com/chriddyp/' +
+    '5d1ea79569ed194d432e56108a04d188/raw/' +
+    'a9f9e8076b837d541398e999dcbac2b2826a81f8/'+
+    'gdp-life-exp-2007.csv')
+
+
+app.layout = html.Div([
+    dcc.Graph(
+        id='life-exp-vs-gdp',
+        figure={
+            'data': [
+                go.Scatter(
+                    x=df[df['continent'] == i]['gdp per capita'],
+                    y=df[df['continent'] == i]['life expectancy'],
+                    text=df[df['continent'] == i]['country'],
+                    mode='markers',
+                    opacity=0.7,
+                    marker={
+                        'size': 15,
+                        'line': {'width': 0.5, 'color': 'white'}
+                    },
+                    name=i
+                ) for i in df.continent.unique()
+            ],
+            'layout': go.Layout(
+                xaxis={'type': 'log', 'title': 'GDP Per Capita'},
+                yaxis={'title': 'Life Expectancy'},
+                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+                hovermode='closest'
+            )
+        }
+    )
+])
+
+# in order to work on shinyproxy
+# see https://support.openanalytics.eu/t/what-is-the-best-way-of-delivering-static-assets-to-the-client-for-custom-apps/363/5
+app.config.suppress_callback_exceptions = True
+app.config.update({
+    'routes_pathname_prefix': os.environ['SHINYPROXY_PUBLIC_PATH'],
+    'requests_pathname_prefix': os.environ['SHINYPROXY_PUBLIC_PATH']
+})
+
+if __name__ == '__main__':
+    app.run_server(port=8050)
